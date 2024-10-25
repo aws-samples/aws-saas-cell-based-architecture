cacfc9e3c944b6457c53072d0272db3400c56081
Mark Jones <jonesaws@amazon.com>
2024-10-25 11:14:31 +1100

    updateCell basic implementation

--- lib/saas-management/control-plane/src/lambdas/UpdateCell/updateCell.py
+++ lib/saas-management/control-plane/src/lambdas/UpdateCell/updateCell.py
@@ -5,6 +5,10 @@ import os
 import boto3
 import logging
 
+# Initialize clients
+dynamodb = boto3.resource('dynamodb')
+
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -18,23 +22,61 @@ def handler(event, context):
     if body:
         try:
             data = json.loads(body)
-            logger.info('Request body: %s', data)
+            if "WaveNumber" in data or "CellName" in data:
+                logger.info('Request body: %s', data)
+
+                cell_management_table = os.environ.get('CELL_MANAGEMENT_TABLE')
+                ddb_table = dynamodb.Table(cell_management_table)
+
+                cell_id = data.get('CellId')
+                cell_name = data.get('CellName',None)
+                cell_wave_number = data.get('WaveNumber',None)
+
+                exp_attr_values = {}                     
+                update_exp = ""
+
+                if cell_name is not None:
+                    update_exp += "set cell_name = :cn"
+                    exp_attr_values[':cn'] = cell_name
+                if cell_wave_number is not None:
+                    if update_exp:
+                        update_exp += ", wave_number = :wn"
+                    else:
+                        update_exp = "set wave_number = :wn"
+                    exp_attr_values[':wn'] = cell_wave_number
+
+                dynamo_response = ddb_table.update_item(
+                    Key={
+                        'PK': cell_id
+                    },
+                    UpdateExpression=update_exp,
+                    ExpressionAttributeValues=exp_attr_values,
+                    ReturnValues="UPDATED_NEW"
+                )
+
+                # Process the request and generate a response
+                response = {
+                    'statusCode': 200,
+                    'body': json.dumps({'CellId': cell_id, 'Status': 'updated'})
+                }
+            else:
+                logger.info('Invalid Request: WaveNumber or CellName not present')
+                response = {
+                    'statusCode': 400,
+                    'body': json.dumps({'error': 'WaveNumber or CellName not present'})
+                }
         except json.JSONDecodeError:
             logger.error('Invalid JSON in request body')
-            return {
+            response = {
                 'statusCode': 400,
                 'body': json.dumps({'error': 'Invalid JSON in request body'})
             }
     else:
-        logger.info('No request body')
-
-    # Process the request and generate a response
-    response = {
-        'statusCode': 200,
-        'body': json.dumps({'message': 'This is a response from the UpdateCell Lambda!'})
-    }
-
+        logger.info('Invalid Request: WaveNumber, CellName or both must be provided')
+        response = {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'WaveNumber, CellName or both must be provided'})
+        }
     # Log the response
     logger.info('Response: %s', response)
-
     return response

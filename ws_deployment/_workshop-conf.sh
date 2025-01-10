d833355b3402b78261cd706e5c7e8457e4e628a2
Mark Jones <jonesaws@amazon.com>
2025-01-10 13:52:23 +1100

    resolving install issues

--- ws_deployment/_workshop-conf.sh
+++ ws_deployment/_workshop-conf.sh
@@ -6,8 +6,11 @@
 
 ## Variables
 REPO_URL="${REPO_URL:=https://github.com/aws-samples/aws-saas-cell-based-architecture}"
-REPO_NAME="${REPO_NAME:=aws-saas-cell-based-architecture}"
 REPO_BRANCH_NAME="${REPO_BRANCH_NAME:=main}"
+REPO_NAME="${REPO_NAME:=aws-saas-cell-based-architecture}"
 TARGET_USER="participant"
 HOME_FOLDER="Workshop"
 DELAY=15 # Used to sleep in functions. Tweak as desired.
+
+echo "Set environment variables for the install:"
+echo "REPO_URL: $REPO_URL, REPO_BRANCH_NAME: $REPO_BRANCH_NAME, REPO_NAME: $REPO_NAME, TARGET_USER: $TARGET_USER, HOME_FOLDER: $HOME_FOLDER, DELAY: $DELAY"

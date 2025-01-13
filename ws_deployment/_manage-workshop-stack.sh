18107cd2a98746d48bdef24a45f788b0050277c8
Mark Jones <jonesaws@amazon.com>
2025-01-13 12:00:52 +1100

    fixed variable replacement in deployment script

--- ws_deployment/_manage-workshop-stack.sh
+++ ws_deployment/_manage-workshop-stack.sh
@@ -12,9 +12,9 @@ create_workshop() {
     echo $VSSERVER_ID "ready"
 
     run_ssm_command "export UV_USE_IO_URING=0 && npm install typescript"
-    run_ssm_command "cd /${HOME_FOLDER} ; git clone --depth 1 --branch ${GIT_BRANCH} ${GIT_REPO} || git pull origin ${GIT_BRANCH}"
-    run_ssm_command "chown -R ${TARGET_USER}:${TARGET_USER} /${HOME_FOLDER}"
-    run_ssm_command ". ~/.bashrc && cd /${HOME_FOLDER}/${REPO_NAME} && chmod +x install.sh && ./install.sh"
-    run_ssm_command "chown -R ${TARGET_USER}:${TARGET_USER} /${HOME_FOLDER}"
+    run_ssm_command "cd /$HOME_FOLDER ; git clone --depth 1 --branch $GIT_BRANCH $GIT_REPO || git pull origin $GIT_BRANCH"
+    run_ssm_command "chown -R $TARGET_USER:$TARGET_USER /$HOME_FOLDER"
+    run_ssm_command ". ~/.bashrc && cd /$HOME_FOLDER/$REPO_NAME && chmod +x install.sh && ./install.sh"
+    run_ssm_command "chown -R $TARGET_USER:$TARGET_USER /$HOME_FOLDER"
 
 }
